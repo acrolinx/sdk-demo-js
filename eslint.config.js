1758c04eeebc4c89efc8ba12e7f8675641361a2f
Thilo Mühlberg <thilo.muehlberg@acrolinx.com>
2024-07-05 18:07:00 +0200

    Update node from 14 to 20

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+const tsEslint = require('typescript-eslint');
+const sonarjs = require('eslint-plugin-sonarjs');
+const sourceTsFiles = ['src/**/*.ts'];
+
+/**
+ * @type {import('eslint').Linter.FlatConfig[]}
+ */
+module.exports = [
+  ...tsEslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: sourceTsFiles,
+  })),
+  {
+    files: sourceTsFiles,
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    plugins: {
+      sonarjs
+    },
+
+    rules: {
+      semi: 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/prefer-regexp-exec': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+    },
+  },
+];
--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,45 @@
-export default {
-  testEnvironment: 'node',
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
+/*
+ * Copyright 2020-present Acrolinx GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$',
+  moduleFileExtensions: ['ts', 'js'],
+  coverageDirectory: 'tmp/coverage',
+  coverageReporters: ['cobertura', 'json', 'lcov', 'text'],
+  collectCoverageFrom: ['src/**/*'],
+  coverageThreshold: {
+    global: {
+      statements: 93.5,
+      branches: 81,
+      functions: 86,
+      lines: 93.7,
     },
   },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    'src/**/*.mts',
-    '!src/**/*.d.ts',
-    '!src/**/*.d.mts',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'tmp',
+        outputName: 'junit.xml',
+      },
+    ],
   ],
+  testURL: 'http://localhost/',
 };
