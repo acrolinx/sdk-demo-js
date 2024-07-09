00b0d6aa6c682bbb51c0aceb6af12b32c8276388
Thilo-Acrolinx <125662076+Thilo-Acrolinx@users.noreply.github.com>
2024-07-09 14:59:35 +0200

    Update node from 14 to 20 (#46)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,25 +1,23 @@
 import { ContentFormat } from '@acrolinx/sdk';
-import { AcrolinxSDKDemo } from '../src/main.js';
+import { AcrolinxSDKDemo } from '../src/main';
 import { uuid } from 'uuidv4';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 describe('SDKDemo tests', () => {
-  // Token generated from the Acrolinx dashboard.
-  const accessToken = 'access-token';
-
-  // URL of the Acrolinx platform.
-  const acrolinxUrl = 'https://partner-dev.internal.acrolinx.sh';
-
-  // Unique signature provided by Acrolinx for the integration.
-  const clientSignature = 'SW50ZWdyYXRpb25EZXZlbG9wbWVudERlbW9Pbmx5';
+  const TEST_SERVER_URL = process.env.TEST_SERVER_URL;
+  const ACROLINX_API_TOKEN = process.env.ACROLINX_API_TOKEN;
+  const ACROLINX_DEV_SIGNATURE = process.env.ACROLINX_DEV_SIGNATURE;
 
   it('test endpoint creation', () => {
-    const demo = new AcrolinxSDKDemo(acrolinxUrl, clientSignature);
+    const demo = new AcrolinxSDKDemo(TEST_SERVER_URL, ACROLINX_DEV_SIGNATURE);
     const endpoint = demo.createEndpoint();
     expect(endpoint).not.toBeNull();
   });
 
   it('test check request creation', () => {
-    const demo = new AcrolinxSDKDemo(acrolinxUrl, clientSignature);
+    const demo = new AcrolinxSDKDemo(TEST_SERVER_URL, ACROLINX_DEV_SIGNATURE);
     const checkRequest = demo.createCheckRequest('gen.demo.' + uuid());
 
     expect(checkRequest).not.toBeNull();
@@ -30,14 +28,14 @@ describe('SDKDemo tests', () => {
   });
 
   it('test running check', async () => {
-    const demo = new AcrolinxSDKDemo(acrolinxUrl, clientSignature);
+    const demo = new AcrolinxSDKDemo(TEST_SERVER_URL, ACROLINX_DEV_SIGNATURE);
     const endpoint = demo.createEndpoint();
     const checkRequest = demo.createCheckRequest('gen.demo.' + uuid());
 
     const checkResult = await demo.checkWithAcrolinx(
       endpoint,
       checkRequest,
-      accessToken,
+      ACROLINX_API_TOKEN,
     );
     expect(checkResult).not.toBeNull();
     expect(checkResult.quality.score).not.toBeNull();
@@ -45,21 +43,21 @@ describe('SDKDemo tests', () => {
     console.log('Acrolinx Score: ' + checkResult.quality.score);
     console.log('Scorecard Url: ' + checkResult.reports.scorecard.link);
 
-    const contentAnalysisDashboardLink = await demo.fetchContentAnalysisDashboardUrl(endpoint, accessToken, checkRequest.checkOptions.batchId);
+    const contentAnalysisDashboardLink = await demo.fetchContentAnalysisDashboardUrl(endpoint, ACROLINX_API_TOKEN, checkRequest.checkOptions.batchId);
 
     expect(contentAnalysisDashboardLink).not.toBeNull();
     console.log('Content Analysis Dashboard Link: ' + contentAnalysisDashboardLink);
-  });
+  }, 10_000);
 
   it('test running check for word document', async () => {
-    const demo = new AcrolinxSDKDemo(acrolinxUrl, clientSignature);
+    const demo = new AcrolinxSDKDemo(TEST_SERVER_URL, ACROLINX_DEV_SIGNATURE);
     const endpoint = demo.createEndpoint();
     const checkRequest = demo.createWordCheckRequest('gen.demo.' + uuid());
 
     const checkResult = await demo.checkWithAcrolinx(
       endpoint,
       checkRequest,
-      accessToken,
+      ACROLINX_API_TOKEN,
     );
     expect(checkResult).not.toBeNull();
     expect(checkResult.quality.score).not.toBeNull();
@@ -67,19 +65,19 @@ describe('SDKDemo tests', () => {
     console.log('Acrolinx Score: ' + checkResult.quality.score);
     console.log('Scorecard Url: ' + checkResult.reports.scorecard.link);
 
-    const contentAnalysisDashboardLink = await demo.fetchContentAnalysisDashboardUrl(endpoint, accessToken, checkRequest.checkOptions.batchId);
+    const contentAnalysisDashboardLink = await demo.fetchContentAnalysisDashboardUrl(endpoint, ACROLINX_API_TOKEN, checkRequest.checkOptions.batchId);
 
     expect(contentAnalysisDashboardLink).not.toBeNull();
     console.log('Content Analysis Dashboard Link: ' + contentAnalysisDashboardLink);
   });
 
   it('test fetching platform capabilities', async () => {
-    const demo = new AcrolinxSDKDemo(acrolinxUrl, clientSignature);
+    const demo = new AcrolinxSDKDemo(TEST_SERVER_URL, ACROLINX_DEV_SIGNATURE);
     const endpoint = demo.createEndpoint();
 
     const capabilities = await demo.getPlatformCapabilities(
       endpoint,
-      accessToken,
+      ACROLINX_API_TOKEN,
     );
     expect(capabilities).not.toBeNull();
     expect(capabilities.contentFormats).not.toBeNull();
@@ -87,6 +85,5 @@ describe('SDKDemo tests', () => {
     capabilities.contentFormats.forEach( (format: ContentFormat) => {
       console.log('Format: ' + format.displayName + ' id: ' + format.id);
     })
-    
   });
 });
